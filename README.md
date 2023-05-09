e3dc3e89e538153d7fee8d7ed2fc54efe5962d87
ejcanoy <euan.james.canoy@gmail.com>
2023-05-09 09:15:44 -0700

    basic outline for js code

--- README.md
+++ README.md
@@ -8,38 +8,41 @@
     - How to give users the option to select if they wan to play with someone or AI
     - 
 - [] create objects (write functions)
-    - [] gameBoard
+    - [x] gameBoard
         - is in charge of keeping track of the game and the actions required to play
-        - [] public 
-            - [] getBoard
-            - [] addXorO
-            - [] getActivePlayer
-            - [] resetBoard
-            - [] isGameOver
-            - [] startGame
+        - [x] public 
+            - [x] board
+            - [x] addXorO 
+            - [x] resetBoard
+        - [x] private
+            - [x] canPlace
+    - [x] gameController
+        - variables
+            - [x] curBoard
+            - [x] player1
+            - [x] player2
+            - [x] number of plays
+        - [] public
+            - [x] startGame
+            - [x] playRound
         - [] private
-            - [] canPlace
+            - [x] isGameOver
+            - [x] calculate winner
+            - [x] calculate tie
+            - [x] getActivePlayer
+    - [x] players
+        - [x] private
+        - [x] public
+            - [x] name
+                - [x] getName
+            - [x] type
+                - [x] getType
     - [] displayController
         - is in charge of creating the userinterface and updating it
         - [] public
             - [] displayBoard
             - [] displayActivePlayer
             - [] playRound
-    - [] gameController
-        - variables
-            -
-        - [] public
-            - [] playRound,
-            - [] getActivePlayer,
-            - [] getWinner
-
-    - [] players
-        - [] private
-        - [] public
-            - [] name
-                - [] getName
-            - [] type
-                - [] getType
 - [] layout
     - [] basic html page 
     - [] javascript interactive x and o
--- app.js
+++ app.js
@@ -1,63 +1,137 @@
-/*
+let blankBoard = new Array(3);
 
-const gameBoard = (player1, player2) => {
-    const board = 2d array 3x3
-    const player1 = player1
-    const player2 = player2
-    
-
-    PUBLIC
-    startGame
-        give user option to choose between AI or 2 player game
-
-
-    getBoard
-        return board
-
-    addXorO
-        canPlace
-
-    getActivePlayer
-
-
-
-
-    PRIVATE
-    canPlace
-        check if place is taken with an x or o
+for(let i = 0; i < blankBoard.length; i++){
+    blankBoard[i] = new Array(3);
+    for (j = 0; i < blankBoard[i].length; i++) {
+        blankBoard[i][j] = "";
+    }
 }
 
-const gameController = () => {
-    const curBoard;
 
-    startGame
-        figure out how we store gameBoard
-
-    playRound
-
-}
-
+blankPlayer = player( "", "", "");
 
 const player = ( name, type, icon) => {
-    const name
-    const type
-    const icon
+    const name = name;
+    const type = type;
+    const icon = icon;
 
-    getIcon
-        return icon
+    return {
+        name,
+        type,
+        icon
+    }
+}
 
-    getName
-        return name
+const initialNumTurns = 0;
 
-    getType
-        return type
+const gameBoard = (() => {
+    const board = blankBoard;
 
+    const canPlace = (location) => {
+        return board[location[0], location[1]] === "";
+    }
+
+    const addXorO = (type, location) => {
+        if (canPlace(location)) {
+            board[location[0], location[1]] = type;
+            return true;
+        }
+        return false;
+    }
+
+    const resetBoard = () => {
+        board = blankBoard;
+    }
+
+    return {
+        board,
+        addXorO,
+        resetBoard
+    }
+})()
+
+
+const gameController = (() => {
+    let curBoard;
+    let player1;
+    let player2;
+    let numberOfPlays;
+
+    const startGame = () => {
+        numberOfPlays = 0;
+        player1 = player("player 1", "player", "X");
+        player2 = player("player 2", "player", "O");
+        curBoard = gameBoard();
+    }
+
+    const playRound = (location) => {
+        if (isGameOver()) return;
+        const icon = (getActivePlayer()).icon;
+        curBoard.addXorO(icon);
+    }
+
+    getActivePlayer = () => {
+        if (numberOfPlays % 2) {
+            return player1;
+        } else {
+            return player2;
+        }
+    }
+
+    const isGameOver = () => {
+        return (calculateWinner() || calculateTie());
+    }
+
+    const calculateWinner = () => {
+        const winner = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        for (let i = 0; i < winner.length; i++) {
+            const [a, b, c] = lines[i];
+            if (curBoard[a] && curBoard[a] === curBoard[b] && curBoard[a] === curBoard[c]) {
+                return curBoard[a];
+            }
+        }
+
+        return null;
+    }
+
+    const calculateTie = () => {
+        for (let i = 0; i < curBoard.length; i++) {
+            if (curBoard[i] === "") return null;
+        }
+        return true;
+    }
+
+    return {
+        startGame,
+        playRound,
+    }
+
+
+})()
+
+
+// const displayController = (() => {
     
+//     // displayBoard
 
-}
+//     // displayActivePlayer
 
-const displayController = () => {
+//     const handleClick = () => {
+//         // if there is something there or isWinner?
+//     }
 
-}
+// })()
 
-*/
+
+
+
